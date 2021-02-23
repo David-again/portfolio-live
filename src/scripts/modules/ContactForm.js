0f1862fdbd78f88581bd564e9f3abf3e3831e467
David-again <d.ogunbanjo@yahoo.com>
2021-02-22 20:31:00 -0500

    Feedback form implemented in React + Netlify form handling

--- src/scripts/modules/ContactForm.js
+++ src/scripts/modules/ContactForm.js
@@ -7,19 +7,20 @@ function ContactForm() {
     return (
       <div>
 
-        <form name="feedback" method="POST" data-netlify="true" className="feedback-form">
+        <form name="feedback" method="POST" className="feedback-form">
             <div className="feedback-form-content">
+              <input type="hidden" name="form-name" value="feedback"/>
                 {/* <label for="feedback-name">Your Name</label> */}
-                <input type="text" name="feedback-name" id="" placeholder="Name" />
+                <input type="text" name="feedback-name" placeholder="Name" />
                 
                 {/* <label for="feedback-email"> Your email address</label> */}
-                <input type="email" name="feedback-email" id="" placeholder="Your email address" />
+                <input type="email" name="feedback-email" placeholder="Your email address" />
 
                 {/* <label for="feedback-subject"> Subject </label> */}
-                <input type="text" name="feedback-subject" id="" placeholder="Subject"/>
+                <input type="text" name="feedback-subject" placeholder="Subject"/>
 
                 {/* <label for="feedback-message"> Your message </label> */}
-                <textarea name="feedback-message" id="" cols="40" rows="10" placeholder="Your message here" />
+                <textarea name="feedback-message" cols="40" rows="10" placeholder="Your message here" />
 
                 <button type="submit" className="feedback-submit-button">Send Feedback</button>
             </div>
