65afc6868f822bd54d6bc05290f1f85ddbde0fbb
David Ogunbanjo <davidogunbanjo@Davids-MacBook-Air.local>
2022-09-27 21:13:46 -0400

    Skills section to ReactJS component - finished

--- src/scripts/components/Skill_Item.js
+++ src/scripts/components/Skill_Item.js
@@ -10,12 +10,12 @@ class Skill_Item extends React.Component{
         // Create an array of consecutive integers from 1 to props.level
         let starsArr = Array.from({length: props.level}, (_, i) => i + 1)
         
-        // Create a solid star for every element in the array
+        // Create a dark-colored star for every element in the array
         const solidStars = starsArr.map((star) => 
             <i key={star.toString()} className="fa fa-star icon__star--themeDark" aria-hidden="true"></i>
         ); 
         
-        // Create light stars for the balance
+        // Create light-colored stars for the balance
         let balArr = Array.from({length: (5 - props.level)}, (_, i) => i + 1)
         const balStars = balArr.map((star) =>
             <i key={star.toString()} className="fa fa-star icon__star--themeLight" aria-hidden="true"></i>
